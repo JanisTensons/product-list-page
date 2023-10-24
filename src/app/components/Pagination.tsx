ede1506a0a2df15a89e68554343c7819c2ce5b86
Janis Tensons <jtensons@gmail.com>
2023-10-24 17:10:38 +0300

    Removed prev, next pages; added styles for current page

--- src/app/components/Pagination.tsx
+++ src/app/components/Pagination.tsx
@@ -4,8 +4,6 @@ import { PaginationProps } from "../../../types";
 const Pagination: React.FC<PaginationProps> = ({
   productsPerPage,
   totalProducts,
-  previousPage,
-  nextPage,
   currentPage,
   paginate,
 }) => {
@@ -24,7 +22,7 @@ const Pagination: React.FC<PaginationProps> = ({
               key={number}
               className={`flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 ${
                 currentPage === number
-                  ? "bg-blue-50 text-blue-600 border-blue-300 dark-bg-gray-700 dark-border-gray-700 dark-text-white"
+                  ? "font-bold bg-gray-100 dark-bg-gray-700 dark-border-gray-700 dark-text-white"
                   : "hover:bg-gray-100 hover-text-gray-700 dark-hover-bg-gray-700 dark-hover-text-white"
               }`}
               onClick={() => paginate(number)}
