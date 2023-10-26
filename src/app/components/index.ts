8173e3b1919ba10ea4a282da4d02aaa25857e0a3
Janis Tensons <jtensons@gmail.com>
2023-10-26 11:44:31 +0300

    Imported all components

--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -1,4 +1,7 @@
 import Pagination from "./Pagination";
 import CustomButton from "./CustomButton";
+import Product from "./Product";
+import ProductsList from "./ProductsList";
+import SearchForm from "./SearchForm";
 
-export { Pagination, CustomButton };
+export { Pagination, CustomButton, Product, ProductsList, SearchForm };
